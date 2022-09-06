4ee97ced7383aeaf56f3fe4facfefa6e56f1ea97
camitoarias <111817139+camitoarias@users.noreply.github.com>
2022-09-05 20:59:48 -0500

    Add files via upload

---
+++ EstanciaCaballos.App.Dominio/entidades/persona.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace  EstanciaCaballos.App.Dominio{
+    public class persona{
+        public string id{get; set;}
+        public string nombre {get; set;}
+        public string apellido {get; set;}
+        public string direccion {get; set;}
+        public string telefono {get; set;}
+        public string correo  {get; set;}
+
+    }
+}
