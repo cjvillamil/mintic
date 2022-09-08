7bbe47adccf718032974dddee4ea8df150dee05a
Carol <cjvillamil.m@gmail.com>
2022-09-07 19:33:13 -0500

    borrado de variables en Caballo.cs

--- EstanciaCaballos.App/EstanciaCaballos.App.Dominio/entidades/Caballo.cs
+++ EstanciaCaballos.App/EstanciaCaballos.App.Dominio/entidades/Caballo.cs
@@ -4,13 +4,11 @@ using System;
 namespace EstanciaCaballos.App.Dominio{
     public class Caballo{
         public int Id {get; set;}//primary key
-        //public Historia historiaCaballo{get;set;}//llave foranea
         public Veterinario veterinarioAsignado{get; set;}
         public Persona propietario{get; set;}
         public string color{get; set;}
         public string raza {get; set;}
         public string especie  {get; set;}   
-        public string variablePrueba {get;set;}
 
     }
 }
