4e83df664e29d7fde18c811656a4e2c94af4f82c
Carol <cjvillamil.m@gmail.com>
2022-09-10 06:18:46 -0500

    Create Finca.cs

---
+++ EstanciaCaballos.App/EstanciaCaballos.App.Dominio/entidades/Finca.cs
@@ -0,0 +1,11 @@
+using System;
+
+
+namespace EstanciaCaballos.App.Dominio{
+    public class Finca{
+        public int Id {get; set;}//primary key
+        public string nombre {get; set;}
+        public string direccion {get; set;}
+        public string telefono {get; set;}
+     }
+}
